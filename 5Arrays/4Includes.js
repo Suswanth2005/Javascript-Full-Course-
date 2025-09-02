5bb2c0cbf359bf202b6b3623ac0816c4f0d8f940
Suswanth2005 <118338243+Suswanth2005@users.noreply.github.com>
2025-09-03 00:05:25 +0530

    03-04-2025

--- 5Arrays/4Includes.js
+++ 5Arrays/4Includes.js
@@ -1,1 +1,14 @@
+//includes checks whether a certain value present in the string or not
 
+let availableSize = ['S','M','L','XL','XXL'];
+const readlinesync = require("readline-sync");
+let input = readlinesync.question("Enter the size you need: ");
+
+let isAvailable = availableSize.includes(input.toUpperCase());
+
+if(isAvailable){
+    console.log("Sure Mam we have that Size");
+}
+else{
+    console.log("Sorry Mam we don't have that size");
+}
